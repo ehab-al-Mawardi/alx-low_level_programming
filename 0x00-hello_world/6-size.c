585204f4d74f30b6eb7caf6a14176103b4585358
ehab-al-Mawardi <ihabalmawardi@gmail.com>
2023-09-07 15:23:30 -0700

    Size is not grandeur, and territory does not make a nation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,27 +1,25 @@
 #include <stdio.h>
 
 /**
-*  main - starting point
+* main - starting point
 *
-*  Description: using size of to print the size of different types. 
+* Description: using size of to print the size of different types.
 *
-*  return: always 0 (Success)
-*
+* Return: Always 0 (Success)
 */
 
 int main(void)
 {
-        char a;
-        int b;
-        long int c;
-        long long int d;
-        float e;
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
 
-        printf("Size of a char:%1u byte(s)\n", sizeof(a));
-        printf("Size of an int:%1u byte(s)\n", sizeof(b));
-        printf("Size of a long int:%1u byte(s)\n", sizeof(c));
-        printf("Size of a long long int:%1u byte(s)\n", sizeof(d));
-        printf("Size of a float:%1u byte(s)\n", sizeof(e));
-        return (0);
-
+	printf("Size of a char:%1u byte(s)\n", sizeof(a));
+	printf("Size of an int:%1u byte(s)\n", sizeof(b));
+	printf("Size of a long int:%1u byte(s)\n", sizeof(c));
+	printf("Size of a long long int:%1u byte(s)\n", sizeof(d));
+	printf("Size of a float:%1u byte(s)\n", sizeof(e));
+	return (0);
 }
