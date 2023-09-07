17e8c15d5d8a253ce05e9bcbc8e04273b8326190
ehab-al-Mawardi <ihabalmawardi@gmail.com>
2023-09-07 11:51:03 -0700

    Hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
-
-void main(void){ 
-
-	puts("Programming is like building a multilingual puzzle \n");
+void main(void){
+	puts("/""Programming is like building a multilingual puzzle");
 	return 0;
 }
