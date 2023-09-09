302b867d56d82e95c62d3321fa3e058757ef8ed1
ehab-al-Mawardi <ihabalmawardi@gmail.com>
2023-09-08 20:10:18 -0700

    Inventing is a combination of brains and materials. The more brains you use, the less material you need

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -22,13 +22,13 @@ int main(void)
 				putchar(digit1 + 48);
 				putchar(digit2 + 48);
 			
-
+			}
 				if (digit1 + digit2 != 17)
 				{
 					putchar(',');
 					putchar(' ');
 				}
-		}
+
 		digit2++;
 	}
 
