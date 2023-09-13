4d6abcc3d7e8a4adbd47655e6978a4605e6208bd
ehab-al-Mawardi <ihabalmawardi@gmail.com>
2023-09-12 19:42:26 -0700

    Even Liber Abbaci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* main - Entry point
+*
+* Description: x + y then the sum of the last two numbers
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int x, y;
+	int sum, prev;
+
+	x = 1;
+
+	y = 2;
+
+	while (x + y <= 100)
+	{
+		sum = x + y
+		prev = sum + y
+	}
+	printf("\n");
+	return (0);
+}
