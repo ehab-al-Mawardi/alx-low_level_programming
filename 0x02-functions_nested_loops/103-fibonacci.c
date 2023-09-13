02f6dcc19964e984b918c885babb2c75afbd2a74
ehab-al-Mawardi <ihabalmawardi@gmail.com>
2023-09-12 20:10:13 -0700

    In computer class, the first assignment was to write a program to print the first 100 Fibonacci numbers. Instead, I wrote a program that would steal passwords of students. My teacher gave me an A

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -11,19 +11,20 @@
 int main(void)
 {
 	unsigned long fib1 = 0, fib2 = 1, sum;
-	float total_sum;
+	unsigned long total_sum = 0;
 
 	while (1)
 	{
 		sum = fib1 + fib2;
-		if (sum > 4000000)
-
+		if (sum >= 4000000)
+			break;
 		if ((sum % 2) == 0)
 			total_sum += sum;
+		sum = fib1 + fib2;
 		fib1 = fib2;
 		fib2 = sum;
 	}
-	printf("%.0f\n", total_sum);
+	printf("%lu\n", total_sum);
 
 	return (0);
 }
---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+* main - Entry point
+*
+* Description:
+*
+* Return: 0 (Success)
+*/
+
+int main(void)
+{
+    int x = 1, y = 2, sum = 0, prev;
+
+    while (x <= 4000000)
+    {
+        if (x % 2 == 0)
+        {
+            sum += x;
+        }
+
+        prev = y;
+        y = x + y;
+        x = prev;
+    }
+
+    printf("Sum of even-valued terms: %d\n", sum);
+
+    return 0;
+}
+
