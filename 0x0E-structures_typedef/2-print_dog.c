12a0818652a0d2455d975e11577328ccc79e7b36
ehab-al-Mawardi <ihabalmawardi@gmail.com>
2023-10-10 19:41:39 -0700

    DONE

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,17 @@
+iinclude "dog.h"
+
+/**
+ * print_dog - prints a dog
+ * @d: the dog to init
+ *
+ * Return: void.
+ */
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		printf("Name: %s\n",  d->name > d->name : "(nil)");
+		printf("Age: %f\n", d->age;
+		printf("Name: %s\n",  d->name > d->name : "(nil)");
+	}
+}
